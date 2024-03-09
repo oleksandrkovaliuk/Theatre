d7cd7510dc33fba0ff6dadcec72642417a4d530a
Alex <sasha1235qaz@ukr.net>
2024-03-09 12:01:10 -0700

    added pop up menu

--- ui/src/components/autorisation/loginOrSignIn/loginSignIn.jsx
+++ ui/src/components/autorisation/loginOrSignIn/loginSignIn.jsx
@@ -2,12 +2,17 @@ import React, { useContext, useEffect, useState } from "react";
 import l from "./loginSignIn.module.scss";
 import { AutorisationMenu } from "../autorisation";
 import { userContext } from "../../../context/userInfoContext";
+import { PopUpMenu } from "../../popUpNavMenu";
+import { UserIcon } from "../../../icons/userIcon";
 
 export const LogInSignIn = () => {
   const [openSignIn, setToOpenSignIn] = useState(false);
   const [openMenu, showAutorisationMenu] = useState(false);
+  const [navMenu, openNavMenu] = useState(false);
+  const [navMenuData, setNavMenuData] = useState(null);
+  const [posForNavMenu, setPostionForNavMenu] = useState(null);
   const [loginned, setLogin] = useState(false);
-  const { user } = useContext(userContext);
+  const { user, setUserInfo } = useContext(userContext);
   const openLogInAutorisationMenu = () => {
     showAutorisationMenu(true);
   };
@@ -22,31 +27,86 @@ export const LogInSignIn = () => {
   const logInUser = () => {
     setLogin(true);
   };
+  const handleOpeningNavMenu = (event, category) => {
+    const position = event.target.getBoundingClientRect();
+    if (position) {
+      setPostionForNavMenu({
+        height: position.height,
+        top: position.top,
+        width: position.width,
+      });
+    }
+    openNavMenu(true);
+  };
+  const closeNavMenu = () => {
+    openNavMenu(false);
+    setPostionForNavMenu(null);
+  };
+  const logOutUser = () => {
+    setUserInfo(null);
+    openNavMenu(false);
+    setNavMenuData([]);
+    setLogin(false);
+  };
   useEffect(() => {
     if (user !== null) {
-      console.log(user , "user")
+      if (user?.role === "admin") {
+        setNavMenuData([
+          "Create event",
+          "Manage events",
+          "Manage news",
+          "Settings",
+        ]);
+      } else {
+        setNavMenuData(["Your events", "Settings"]);
+      }
       setLogin(true);
     }
+    return () => setNavMenuData([]);
   }, [user]);
   return (
     <>
-      {loginned ? (
-        <div>
-          {user?.username}
-          <p>{user?.role === "admin" ? "admin" : "lol"}</p>
-        </div>
-      ) : (
-        <div className={l.logInSignInContainer}>
-          <button onClick={() => openLogInAutorisationMenu()}>Login In</button>
-          <button onClick={() => openSignInAutorisationMenu()}>Sign In</button>
-        </div>
-      )}
       <AutorisationMenu
         show={openMenu ? true : false}
         signIn={openSignIn ? true : false}
         closeMenu={() => handleClosingMenu()}
         loginUser={() => logInUser()}
       />
+      <PopUpMenu
+        logIn={loginned ? true : false}
+        showMenu={navMenu ? true : false}
+        data={navMenuData}
+        top={posForNavMenu?.top}
+        height={posForNavMenu?.height}
+        width={posForNavMenu?.width}
+        closeMenu={() => closeNavMenu()}
+      >
+        {!loginned ? (
+          <div className={l.logInSignInContainer}>
+            <button onClick={() => openLogInAutorisationMenu()}>
+              Login In
+            </button>
+            <div className={l.signInText}>
+              <span>Don't have an account?</span>
+              <button onClick={() => openSignInAutorisationMenu()}>
+                Sign In
+              </button>
+            </div>
+          </div>
+        ) : (
+          <button onClick={() => logOutUser()} className={l.logOutBtn}>
+            Log out
+          </button>
+        )}
+      </PopUpMenu>
+      <div className={l.leftNavWrap}>
+        <button
+          onClick={(event) => handleOpeningNavMenu(event)}
+          className={l.accountBtn}
+        >
+          <UserIcon />
+        </button>
+      </div>
     </>
   );
 };

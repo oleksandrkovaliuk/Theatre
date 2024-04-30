abb115c43a9ada6b62afc50d85794ac7338cee1a
Alex <sasha1235qaz@ukr.net>
2024-04-29 21:22:34 -0600

    cors

--- server/src/configs/corsSettings.js
+++ server/src/configs/corsSettings.js
@@ -1,10 +1,11 @@
-const cors = require('cors');
+const cors = require("cors");
 
-const defaultCorsOptions = cors.CorsOptions = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-};
+const defaultCorsOptions = (cors.CorsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+});
 
 module.exports = defaultCorsOptions;
